ae9fef25198391d68dfd9c1e7636914ea5e89c6e
ru8anraj <ru8anraj@gmail.com>
2018-03-26 17:17:02 +0530

    mark all features added

--- src/client/components/ToDo.jsx
+++ src/client/components/ToDo.jsx
@@ -14,18 +14,21 @@ export default class ToDo extends React.Component{
         { name: 'Node', checked: false, strike: 'none' },
         { name: 'Express', checked: false, strike: 'none' }
       ],
-      openSnack: false
+      openSnack: false,
+      snackMsg: '',
+      markAll: 'complete'
     };
     this.handleNewToDo = this.handleNewToDo.bind(this);
     this.toggleStrikeIt = this.toggleStrikeIt.bind(this);
     this.handleClearFinished = this.handleClearFinished.bind(this);
     this.deleteCurrentToDo = this.deleteCurrentToDo.bind(this);
+    this.handleMarkAll = this.handleMarkAll.bind(this);
   }
 
   timeOutSnackBar(){
     setTimeout(() => {
-      this.setState({openSnack: false});
-    }, 1500);
+      this.setState({openSnack: false, snackMsg: ''});
+    }, 1000);
   }
 
   handleNewToDo(value){
@@ -40,9 +43,13 @@ export default class ToDo extends React.Component{
     if (todoList[i].checked) {
       todoList[i].checked = false;
       todoList[i].strike = 'none';
+      this.setState({openSnack: true, snackMsg: 'Task marked as incomplete'});
+      this.timeOutSnackBar();
     } else {
       todoList[i].checked = true;
       todoList[i].strike = 'line-through';
+      this.setState({openSnack: true, snackMsg: 'Task marked as complete'});
+      this.timeOutSnackBar();
     }
     this.setState({todoList: todoList});
   }
@@ -52,8 +59,7 @@ export default class ToDo extends React.Component{
     if (todoList[i].checked) {
       todoList.splice(i, 1);
     } else {
-      console.log('other');
-      this.setState({openSnack: true});
+      this.setState({openSnack: true, snackMsg: 'Task not checked yet!'});
       this.timeOutSnackBar();
     }
     this.setState({todoList: todoList});
@@ -67,6 +73,38 @@ export default class ToDo extends React.Component{
     this.setState({todoList: todoList});
   }
 
+  handleMarkAll(){
+    var { todoList } = this.state;
+    var { markAll } = this.state;
+    if (this.state.markAll == 'complete') {
+      for (var i = 0; i < todoList.length; i++) {
+        todoList[i].checked = true;
+        todoList[i].strike = 'line-through';
+      }
+      markAll = 'incomplete';
+      this.setState({
+        todoList: todoList,
+        markAll: markAll,
+        openSnack: true,
+        snackMsg: 'All tasks marked as complete'
+      });
+      this.timeOutSnackBar();
+    } else {
+      for (var i = 0; i < todoList.length; i++) {
+        todoList[i].checked = false;
+        todoList[i].strike = 'none';
+      }
+      markAll = 'complete';
+      this.setState({
+        todoList: todoList,
+        markAll: markAll,
+        openSnack: true,
+        snackMsg: 'All tasks marked as incomplete'
+      });
+      this.timeOutSnackBar();
+    }
+  }
+
   render(){
     return(
       <div>
@@ -74,6 +112,12 @@ export default class ToDo extends React.Component{
         <Segment raised className='ToDo-Card'>
           <center>
             <AddToDo handleNewToDo={this.handleNewToDo} />
+          </center>
+          <br />
+          <center>
+            <Button onClick={this.handleMarkAll}>
+              <span> Mark all as {this.state.markAll} </span>
+            </Button>
           </center>
           <ListToDo
             todoList={this.state.todoList}
@@ -84,8 +128,8 @@ export default class ToDo extends React.Component{
             <Button onClick={this.handleClearFinished}>Clear Finished</Button>
           </center>
         </Segment>
-        <TransitionablePortal className='snackbar' open={this.state.openSnack} transition={{ animation:'swing up', duration: '500'}}>
-          <span>SnackBar...</span>
+        <TransitionablePortal className='snackbar' open={this.state.openSnack} transition={{ animation:'swing up', duration: '100'}}>
+          <span>{this.state.snackMsg}</span>
         </TransitionablePortal>
       </div>
     );
